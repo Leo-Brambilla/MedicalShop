dd3601a638a69f2255f5453394d2242a23afc7e6
leonardo <leonardo.araujo@exago.com>
2024-05-15 23:35:47 -0300

    Spring Boot configuration

---
+++ medicalshop/src/main/java/com/leobrambilla/medicalshop/MedicalshopApplication.java
@@ -0,0 +1,13 @@
+package com.leobrambilla.medicalshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalshopApplication.class, args);
+	}
+
+}
